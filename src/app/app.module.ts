648f5dab3dfdf055cf0572f58e12f07903768f3a
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2019-12-17 11:05:27 -0300

    Acrescenta JSON Server ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule, TooltipModule, ModalModule, BsDatepickerModule } from 'ngx-bootstrap';
+import { AlertModule } from 'ngx-bootstrap/alert';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,22 +11,40 @@ import { AppComponent } from './app.component';
 import { PaginaInicialComponent } from './components/pagina-inicial/pagina-inicial/pagina-inicial.component';
 import { PaginaInicialRoutingModule } from './components/pagina-inicial/pagina-inicial-routing.module';
 import { NavComponent } from './components/nav/nav.component';
+import { ProdutoRoutingModule } from './components/produto/produto-routing.module';
+import { ProdutoComponent } from './components/produto/cadastro-produto/produto.component';
+import { CurrencyMaskModule } from "ng2-currency-mask";
+import { CategoriaComponent } from './components/categoria/cadastro-categoria/categoria.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CategoriaRoutingModule } from './components/categoria/categoria-routing.module';
+import { CadastroCategoriaAdmComponent } from './components/categoria/cadastro-categoria-adm/cadastro-categoria-adm.component';
+import { LoginComponent } from './components/pagina-inicial/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PaginaInicialComponent,
-    NavComponent
+    NavComponent,
+    ProdutoComponent,
+    CategoriaComponent,
+    FooterComponent,
+    CadastroCategoriaAdmComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ModalModule.forRoot(),
     BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    PaginaInicialRoutingModule
+    PaginaInicialRoutingModule,
+    ProdutoRoutingModule,
+    CurrencyMaskModule,
+    CategoriaRoutingModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
