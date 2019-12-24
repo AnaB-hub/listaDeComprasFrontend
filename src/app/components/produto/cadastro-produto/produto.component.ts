27745573873189c3a9aee074dcfcde3c34082d15
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2019-12-24 12:57:32 -0300

    Ajustes referente ao componente de cadastro de produtos

--- src/app/components/produto/cadastro-produto/produto.component.ts
+++ src/app/components/produto/cadastro-produto/produto.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ProdutoService } from '../service/produto.service';
 import { Mensagens } from 'src/utils/Mensagens.enum';
 import { CategoriaService } from '../../categoria/service/categoria.service';
+import { ProdutoSugeridoService } from '../service/produto-sugerido.service';
 
 @Component({
   selector: 'app-produto',
@@ -23,7 +23,7 @@ export class ProdutoComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private service: ProdutoService,
+    private service: ProdutoSugeridoService,
     private categoriaService: CategoriaService
   ) { }
 
@@ -36,9 +36,8 @@ export class ProdutoComponent implements OnInit {
     this.cadastroForm = this.formBuilder.group({
       'id': [null],
       'nome': [null, Validators.required],
-      'categoria': [null, Validators],
-      'descricao': [null],
-      'ativo': [true]
+      'categoriaId': [null, Validators],
+      'descricao': [null]
     })
   }
 
@@ -51,50 +50,17 @@ export class ProdutoComponent implements OnInit {
   salvar(): void {
     if (this.cadastroForm.valid) {
       let produto = this.cadastroForm.value;
-      if (produto.id) {
-        this.service.alterar(produto).subscribe(_ => {
-          this.mensagemParaUsuario('Produto alterado com sucesso!', true, 3000);
-          this.limpar();
-        });
-      } else {
-        this.service.cadastrar(produto).subscribe(_ => {
-          this.mensagemParaUsuario('Produto cadastrado com sucesso!', true, 3000);
-          this.limpar();
-        });
-      }
+      this.service.cadastrar(produto).subscribe(_ => {
+        this.mensagemParaUsuario('Obrigado pela sugestão!', true, 3000);
+        this.limpar();
+      });
     } else {
       this.mensagemParaUsuario(Mensagens.CAMPO_OBRIGATORIO, false, 5000);
     }
   }
 
-  preencherFormularioEdicao(produto): void {
-    if (produto) {
-      this.cadastroForm.setValue({
-        id: produto.id,
-        nome: produto.nome,
-        categoria: produto.categoria || null,
-        descricao: produto.descricao  || null,
-        ativo: produto.ativo
-      });
-    }
-  }
-
-  editar(produto): void {
-    this.preencherFormularioEdicao(produto);
-  }
-
-  excluir(produtoId): void {
-    if (produtoId) {
-      this.service.excluir(produtoId).subscribe(_ => {
-        this.mensagemParaUsuario('Produto excluído com sucesso!', true, 3000);
-        this.carregarProdutos();
-      });
-    }
-  }
-
   limpar(): void {
     this.cadastroForm.reset();
-    this.carregarProdutos();
   }
 
   sugerirCategoria() {
@@ -107,23 +73,6 @@ export class ProdutoComponent implements OnInit {
     setTimeout(() => {
       this.mensagem = '';
     }, timeout);
-  }
-
-  apresentarProdutos(): void {
-    this.carregarProdutos();
-    this.apresentarLista = !this.apresentarLista;
-  }
-
-  async carregarProdutos(): Promise<void> {
-    let produtos = await this.service.produtos().toPromise();
-    this.produtos = [];
-    for (let i = 0; i < produtos.length; i++) {
-      if (this.categorias && this.categorias.length > 0) {
-        let categoria = this.categorias.find(x => x.id == produtos[i].categoria);
-        categoria ? produtos[i].categoriaDescricao = categoria.nome : null;
-      }
-      this.produtos.push(produtos[i]);
-    }
   }
 
 }
--- src/app/components/produto/produto-routing.module.ts
+++ src/app/components/produto/produto-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProdutoComponent } from './cadastro-produto/produto.component';
+import { AprovarProdutoComponent } from './aprovar-produto/aprovar-produto.component';
 
 const routes: Routes = [
   {
     path: 'operador/cadastro-produto',
     component: ProdutoComponent
+  },
+  {
+    path: 'adm/aprovar-produto',
+    component: AprovarProdutoComponent
   }
 ]
 
