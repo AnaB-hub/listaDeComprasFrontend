cbac3ae5ac3351be231bc13d3e5af95bee2579dd
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2019-12-24 12:59:12 -0300

    Acrescenta filtro por nome do produto no cadastro de lista de compras

--- src/app/components/compras/lista-compras/lista-compras.component.ts
+++ src/app/components/compras/lista-compras/lista-compras.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProdutoService } from '../../produto/service/produto.service';
 import { CategoriaService } from '../../categoria/service/categoria.service';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Mensagens } from 'src/utils/Mensagens.enum';
 
 @Component({
   selector: 'app-lista-compras',
@@ -12,24 +13,54 @@ export class ListaComprasComponent implements OnInit {
 
   //listas
   produtos: any[] = [];
+  produtosFiltrados: any[] = [];
   categorias: any[] = [];
   produtosSelecionados: any[] = [];
 
   //strings
   classeMensagem: string = 'alert-success';
   mensagem: string ='';
+  nomeLista: string = '';
 
   //formulário
   cadastroForm: FormGroup;
 
   constructor(
     private produtoService: ProdutoService,
-    private categoriaService: CategoriaService
+    private categoriaService: CategoriaService,
+    private formBuilder: FormBuilder
   ) { }
 
+  //encapsulamente: acessa os metodos e não a variavel
+  _filtroLista: string; //propriedade
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string){
+    this._filtroLista = value;
+    this.produtosFiltrados = this.filtroLista ? this.filtrarProduto(this.filtroLista) : this.produtos;
+  }
+
   async ngOnInit() {
+    this.inicializarFormulario();
     await this.carregarCategorias();
     await this.carregarListaProdutos();
+    this.produtosFiltrados = this.produtos;
+  }
+
+  filtrarProduto(filtrarPor: string){
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.produtos.filter(
+      produto => produto.nome.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  inicializarFormulario() {
+    this.cadastroForm = this.formBuilder.group({
+      'id': [null],
+      'nome': [null, Validators.required],
+      'produtos': [null]
+    })
   }
 
   async carregarCategorias(): Promise<void> {
@@ -42,7 +73,7 @@ export class ListaComprasComponent implements OnInit {
     this.produtos = [];
     for (let i = 0; i < produtos.length; i++) {
       if (this.categorias && this.categorias.length > 0) {
-        let categoria = this.categorias.find(x => x.id == produtos[i].categoria);
+        let categoria = this.categorias.find(x => x.id == produtos[i].categoriaId);
         categoria ? produtos[i].categoriaDescricao = categoria.nome : null;
         this.produtos.push(produtos[i]);
       }
@@ -77,7 +108,13 @@ export class ListaComprasComponent implements OnInit {
   }
 
   salvarLista(): void {
-
+    if (this.cadastroForm.valid) {
+      let lista = this.cadastroForm.value;
+      lista.produtos = this.produtosSelecionados;
+      console.log(lista);
+    } else {
+      this.mensagemParaUsuario('O campo Nome da Lista é obrigatório', false, 5000);
+    }
   }
 
   mensagemParaUsuario(mensagem: string, sucesso: boolean, timeout: number): void {
@@ -86,6 +123,14 @@ export class ListaComprasComponent implements OnInit {
     setTimeout(() => {
       this.mensagem = '';
     }, timeout);
+  }
+
+  mudarTituloLista(nomeLista) {
+    if (nomeLista) {
+      this.nomeLista = nomeLista;
+    } else {
+      this.nomeLista = '';
+    }
   }
 
 }
